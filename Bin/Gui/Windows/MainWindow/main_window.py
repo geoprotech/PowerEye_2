bf426eeb31cff4f1ea0d243bc23bdffe38a83bce
TMN <super.rikunen@gmail.com>
2023-12-13 16:18:55 +0300

    Refactor

--- Bin/Gui/Windows/MainWindow/main_window.py
+++ Bin/Gui/Windows/MainWindow/main_window.py
@@ -2,8 +2,10 @@ from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QHBoxLayout, QLabel, QMainWindow
 
 from Src.Icons import ICONS_PATH
-from Src.Styles.stylesheet import (BASE_STYLESHEET,
-                                   MAIN_WINDOW_HEADER_STYLESHEET)
+from Src.Styles.stylesheet import (
+    BASE_STYLESHEET,
+    MAIN_WINDOW_HEADER_STYLESHEET,
+)
 
 
 class MainWindow(QMainWindow):

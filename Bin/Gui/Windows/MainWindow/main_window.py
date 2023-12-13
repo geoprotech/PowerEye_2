d1e7d2415914617ea07b3eb57c439bdd5c2c2b13
TMN <super.rikunen@gmail.com>
2023-12-13 14:47:25 +0300

    Add favicon, refactor main window

--- Bin/Gui/Windows/MainWindow/main_window.py
+++ Bin/Gui/Windows/MainWindow/main_window.py
@@ -1,5 +1,7 @@
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QHBoxLayout, QLabel, QMainWindow
 
+from Src.Icons import ICONS_PATH
 from Src.Style.stylesheet import BASE_STYLESHEET, MAIN_WINDOW_HEADER_STYLESHEET
 
 
@@ -8,19 +10,18 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.setGeometry(*BASE_STYLESHEET.geometry())
         self.setStyleSheet(repr(BASE_STYLESHEET))
-
         self.setWindowTitle("PowerEye 2.0")
-        self.header = QLabel(self)
-
-        self.header.setStyleSheet(repr(MAIN_WINDOW_HEADER_STYLESHEET))
-
-        self.header.setGeometry(
-            0, 0, self.width() // 2, self.height() // 100 * 10
-        )
-        self.header.setText("PowerEye")
-        self.header.setFixedHeight(80)
+        self.setWindowIcon(QIcon(str(ICONS_PATH.joinpath("skyrus_logo.png"))))
         self.box_layout = QHBoxLayout()
-        self.box_layout.addWidget(self.header)
         self.setLayout(self.box_layout)
 
+        self.header = QLabel(self)
+        self.setup_header()
+        self.box_layout.addWidget(self.header)
+
         self.setCentralWidget(self.header)
+
+    def setup_header(self):
+        self.header.setStyleSheet(repr(MAIN_WINDOW_HEADER_STYLESHEET))
+        self.header.setText("PowerEye")
+        self.header.setFixedHeight(80)
---
+++ Src/Icons/__init__.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+ICONS_PATH = Path(__file__).parent

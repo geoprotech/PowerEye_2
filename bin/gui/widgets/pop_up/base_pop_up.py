15af47e0411c4882f45954a55666dfb9fada2caf
Artem <artiebears@mail.ru>
2024-02-03 19:01:36 +0300

    refactor code

--- bin/gui/widgets/pop_up/base_pop_up.py
+++ bin/gui/widgets/pop_up/base_pop_up.py
@@ -1,34 +1,47 @@
-from abc import abstractmethod
-from PySide6.QtWidgets import QDialog, QLabel, QWidget, QPushButton, QVBoxLayout
-from PySide6.QtCore import Qt
-from bin.gui.widgets.layouts import MainWorkspaceLayout, VerticalLayout, HeaderLayout
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QVBoxLayout, QLabel, QHBoxLayout
 import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QVBoxLayout, QWidget
+
+from bin.gui.widgets.layouts import HeaderLayout, VerticalLayout
+from bin.gui.widgets.stub import Color
 
 
 class PopUp(QDialog):
-    def __init__(self,
-                 parent: QWidget,
-                 title: str,
-                 size: tuple):
+    def __init__(self, parent: QWidget, title: str, size: tuple):
         super().__init__(parent=parent)
-        self.set_up(title, size)
-        self.show_popup()
+        self._layout_main = QVBoxLayout()
+        self._layout_main.setContentsMargins(0, 0, 0, 0)
+        self._layout = VerticalLayout(parent=self)
+        self._layout.set_content_margins(0, 0, 0, 0)
+        self._layout.add_widget(HeaderLayout(parent=self))
+        self._layout.add_widget(Color('red'))
+        self._layout_main.addWidget(self._layout)
+
+        self.setLayout(self._layout_main)
+        # self.add_widget(Color("red"))
+
         self.make()
+        self.show()
+        # self.show_popup()
 
     def make(self):
         self.setWindowFlag(Qt.FramelessWindowHint)
-        layout = VerticalLayout(parent=self)
-        layout.add_widget(HeaderLayout(parent=self))
+        self.setFixedWidth(200)
+        self.setFixedHeight(200)
 
     def set_up(self, title: str, size: tuple):
-        self.setWindowFlag(Qt.FramelessWindowHint)
+        # self.setWindowFlag(Qt.FramelessWindowHint)
+        pass
 
     def popup_control_buttons(self, *args, **kwargs):
         pass
 
     def show_popup(self):
         self.exec()
+
+    def add_widget(self, widget: QWidget):
+        self._layout.add_widget(widget)
 
     #
     # @abstractmethod
@@ -52,7 +65,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(button)
 
     def sshow_popup(self):
-        tt = PopUp(self, 'Test', (0, 0, 200, 200))
+        PopUp(self, 'Test', (0, 0, 200, 200))
 
 
 def run():

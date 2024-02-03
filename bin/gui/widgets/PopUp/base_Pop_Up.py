2fdbfed922a3090c25de72b50e5b507da250421f
Alix <ali_ramazanov_2000@mail.ru>
2024-02-03 18:24:16 +0300

    init

--- bin/gui/widgets/PopUp/base_Pop_Up.py
+++ bin/gui/widgets/PopUp/base_Pop_Up.py
@@ -1,8 +1,9 @@
 from abc import abstractmethod
 from PySide6.QtWidgets import QDialog, QLabel, QWidget, QPushButton, QVBoxLayout
 from PySide6.QtCore import Qt
-
-import bin.gui.widgets.buttons as buttons
+from bin.gui.widgets.layouts import MainWorkspaceLayout, VerticalLayout, HeaderLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QVBoxLayout, QLabel, QHBoxLayout
+import sys
 
 
 class PopUp(QDialog):
@@ -13,31 +14,56 @@ class PopUp(QDialog):
         super().__init__(parent=parent)
         self.set_up(title, size)
         self.show_popup()
+        self.make()
 
-    def post_setup(self):
-        layoutV = QVBoxLayout()
-        layoutV.addWidget(VerticalLayout(parent = self))
-        self.setLayout(layoutV)
+    def make(self):
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        layout = VerticalLayout(parent=self)
+        layout.add_widget(HeaderLayout(parent=self))
 
     def set_up(self, title: str, size: tuple):
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setGeometry(*size)
+        self.setWindowFlag(Qt.FramelessWindowHint)
 
     def popup_control_buttons(self, *args, **kwargs):
         pass
 
-
     def show_popup(self):
         self.exec()
 
-    @abstractmethod
-    def make(self):
-        pass
+    #
+    # @abstractmethod
+    # def add_buttons(self, button:QPushButton, *args, **kwargs):
+    #     pass
+    #
+    # @abstractmethod
+    # def add_widget(self, widget: QWidget, *args, **kwargs):
+    #     pass
 
-    @abstractmethod
-    def add_buttons(self, button:QPushButton, *args, **kwargs):
-        pass
 
-    @abstractmethod
-    def add_widget(self, widget: QWidget, *args, **kwargs):
-        pass
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Pop-up окно с PySide6")
+
+        # Создаем кнопку
+        button = QPushButton("Открыть Pop-up окно", self)
+        button.clicked.connect(self.sshow_popup)
+        self.setCentralWidget(button)
+
+    def sshow_popup(self):
+        tt = PopUp(self, 'Test', (0, 0, 200, 200))
+
+
+def run():
+    app = QApplication(sys.argv)
+
+    # Создаем главное окно
+    window = MainWindow()
+    window.show()
+
+    # Запускаем основной цикл обработки событий
+    app.exec()
+
+
+run()

8997d21494402742cf0ef7dde27f7e15c3673723
Alix <ali_ramazanov_2000@mail.ru>
2024-02-08 19:39:21 +0300

    fixed: config PopUPBase

--- bin/gui/widgets/pop_up/base_pop_up.py
+++ bin/gui/widgets/pop_up/base_pop_up.py
@@ -9,15 +9,23 @@ from bin.gui.widgets.layouts import HeaderLayoutPopUp, VerticalLayout
 
 @init_protocol
 class PopUp(QDialog):
-    """ """
+    """
+    Base class PopUp Window
+
+        Args:
+            parent(QWidget): parent
+            title_popup(str): Window title in header
+            size(tuple(x: int, y: int,  width: int, height: int)): PopUP size options
+    """
 
     def __init__(self, parent: QWidget, title: str, size: tuple):
         self.title_popup = title
         self.size = size
         super().__init__(parent=parent)
 
-    def make(self) -> None:
-        self.setMinimumSize(*self.size[2:])
+    def post_setup(self) -> None:
+        self.move(*self.size[:2])
+        self.resize(*self.size[2:])
         self.setWindowFlag(Qt.FramelessWindowHint)
 
         self._layout_main = QVBoxLayout()
@@ -32,56 +40,33 @@ class PopUp(QDialog):
         self._layout_main.addWidget(self._layout)
         self.setLayout(self._layout_main)
 
-    def add_widget(self, widget: QWidget) -> None:
+    @abstractmethod
+    def make(self) -> None:
+        """
+        Basic window designer
+
+            Example:
+                self._add_widget(Color('red'))
+                button = CloseButtonPopUp(self.headerlayoutPopUp, onclick=self.end)
+                self._add_control_buttons(button, alignment=Qt.AlignRight)
+        """
+
+    def _add_widget(self, widget: QWidget) -> None:
         self._layout.add_widget(widget)
 
-    def add_contol_buttons(
+    def _add_control_buttons(
         self, button: QPushButton, stretch: int = 0, alignment: Qt.AlignmentFlag or None = None
     ) -> None:
         self.headerlayoutPopUp.add_button(button, stretch, alignment)
 
-    @abstractmethod
-    def post_setup(self):
-        """ """
+    def _add_widget_header(self, widget: QWidget, stretch: int = 0, alignment: Qt.AlignmentFlag or None = None) -> None:
+        self.headerlayoutPopUp.add_widget(widget, stretch, alignment)
+
+    def show(self):
+        self.exec()
 
     def end(self) -> None:
+        """
+        Closing a PopUP
+        """
         self.accept()
-
-
-"""
-Test
-import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
-from bin.gui.widgets.buttons.pop_up_close_button import CloseButtonPopUp
-from bin.gui.widgets.stub import Color
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("Pop-up окно с PySide6")
-
-        # Создаем кнопку
-        button = QPushButton("Открыть Pop-up окно", self)
-        button.clicked.connect(self.sshow_popup)
-        self.setCentralWidget(button)
-
-    def sshow_popup(self):
-        a = PopUp(self, 'ali', (0, 0, 300, 500))
-        a.add_widget(Color('red'))
-        t = CloseButtonPopUp(a.headerlayoutPopUp, onclick=a.end)
-        a.add_contol_buttons(t)
-
-
-def run():
-    app = QApplication(sys.argv)
-
-    # Создаем главное окно
-    window = MainWindow()
-    window.show()
-
-    # Запускаем основной цикл обработки событий
-    app.exec()
-
-
-run()
-"""

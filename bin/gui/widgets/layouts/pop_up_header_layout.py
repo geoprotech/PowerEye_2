74cc893b442f01c372359a55f69b303789d93391
Artem <artiebears@mail.ru>
2024-02-23 15:52:11 +0300

    refactor pop_up

--- bin/gui/widgets/layouts/pop_up_header_layout.py
+++ bin/gui/widgets/layouts/pop_up_header_layout.py
@@ -1,16 +1,10 @@
-from PySide6.QtWidgets import QDialog
-
 from .base_horizontal_layout import HorizontalLayout
 from src.styles.components.widgets.layouts import HEADER_STYLESHEET
 
 
 class PopUpHeaderLayout(HorizontalLayout):
-    def __init__(self, parent: QDialog):
-        self.parent = parent
-        super().__init__(parent=parent)  # super().__init__ -> -> post_setup -> make -> show
-
     def make(self):
-        self.setObjectName("header")
+        self.setObjectName("pop_up__header")
         self.setStyleSheet(HEADER_STYLESHEET)
         self.set_content_margins(0, 0, 0, 0)
         self.set_spacing(0)

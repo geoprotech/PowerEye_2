7b3fda177a0cac3116ea7880707c6f12783ae5b8
TeeMan508 <t.shshudro@alumni.nsu.ru>
2024-02-28 13:21:08 +0300

    Storage update

--- bin/storage/storage.py
+++ bin/storage/storage.py
@@ -38,7 +38,7 @@ class Storage(object):
             raise StorageException(f"No any signals found for key: '{key}'.")
 
         if signal not in self._data[key]["signals"]:
-            raise StorageException(f"Can't find '{signal.__name__}' for key '{key}'")
+            raise StorageException(f"Can't find signal'{signal.__name__}' for key '{key}'")
 
         return self._data[key]["signals"].remove(signal)
 
@@ -56,3 +56,21 @@ class Storage(object):
             raise StorageException(f"No comparable data for key: {key}")
 
         return self._data[key]["value"]
+
+    def clear(self, key: str) -> None:
+        if key not in self._data:
+            raise StorageException(f"No key found: '{key}'")
+
+        self._data[key]["value"] = None
+
+    def isolate(self, key: str) -> None:
+        if key not in self._data:
+            raise StorageException(f"No key found: '{key}'")
+
+        self._data[key]["signals"] = []
+
+    def remove(self, key: str) -> None:
+        if key not in self._data:
+            raise StorageException(f"No key found: '{key}'")
+
+        self._data.pop(key)

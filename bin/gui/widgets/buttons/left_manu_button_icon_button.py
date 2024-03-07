ff88dedc2cad75b725f364279973ad1abdeae613
Artem <artiebears@mail.ru>
2024-03-07 13:35:56 +0300

    add left menu

--- bin/gui/widgets/buttons/left_manu_button_icon_button.py
+++ bin/gui/widgets/buttons/left_manu_button_icon_button.py
@@ -1,28 +1,31 @@
-from PySide6.QtCore import QSize
+from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import QIcon
 
 from .base import BaseButton
 from src.icons import ICONS_PATH
-from src.styles.components.widgets.buttons import LEFT_MENU_BUTTON_STYLESHEET
+from src.styles.components.widgets.buttons import LEFT_MENU_OVER_BUTTON_STYLESHEET
 
 
 class LeftMenuIconButton(BaseButton):
-    def __init__(self, parent, icon: str, tooltip: str or None = None):
+    def __init__(self, parent, icon: str, tooltip: str or None = None, text: str = ""):
         """
         @param parent: QWidget parent
         @param icon_name: name of file from icons folder
         @param tooltip: string of tooltip (optionally)
         """
         self.__icon = icon
+        self.text = text
         super().__init__(parent=parent, tooltip=tooltip)
 
     def make(self):
+        self.setText(f"   {self.text}")  # for distance between icon and text
         self.setIcon(QIcon(str(ICONS_PATH.joinpath(self.__icon))))
-        self.setStyleSheet(LEFT_MENU_BUTTON_STYLESHEET)
+        self.setStyleSheet(LEFT_MENU_OVER_BUTTON_STYLESHEET)
         size = self.size()
-        icon_width = int(size.width())
-        icon_height = int(size.height())
+        icon_width = 45
+        icon_height = size.height()
         self.setIconSize(QSize(icon_width, icon_height))
+        self.setContentsMargins(0, 0, 0, 0)
 
     def on_emit(self):
         """
--- src/styles/components/widgets/buttons/__init__.py
+++ src/styles/components/widgets/buttons/__init__.py
@@ -1,4 +1,5 @@
 from .window_control_button import *
 from .left_menu_buttons import *
+from .left_menu_over_buttons import LEFT_MENU_OVER_BUTTON_STYLESHEET
 
 # from .control_buttons import *

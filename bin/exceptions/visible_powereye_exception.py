b391fd8f874ae3389147b5071709e89f3b6a7e7f
TeeMan508 <t.shshudro@alumni.nsu.ru>
2024-03-10 22:07:13 +0300

    Reformat stylesheet class implementation

--- bin/exceptions/visible_powereye_exception.py
+++ bin/exceptions/visible_powereye_exception.py
@@ -1,11 +1,13 @@
+from typing import Literal
+
 from . import BasePowereyeException
 
 
 class VisiblePowereyeException(BasePowereyeException):
-    def __init__(self, text: str, level: str) -> None:
+    def __init__(self, text: str, level: Literal["debug", "info", "warn", "error", "critical"]) -> None:
         super(VisiblePowereyeException, self).__init__(text, level)
         self._create_error_window()
 
-    def _create_error_window(self):
+    def _create_error_window(self) -> None:
         # create error window(use BaseWindow class)
         pass

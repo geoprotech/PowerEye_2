b2ec3f8d9f025b4160c2f60fbf223af0c25e4273
Alix <ali_ramazanov_2000@mail.ru>
2024-03-19 11:59:37 +0300

    VisiblePowereyeException:Edits after review

--- bin/exceptions/visible_exception_handler.py
+++ bin/exceptions/visible_exception_handler.py
@@ -4,13 +4,13 @@ from bin.gui.windows import WarningPopUp
 from bin.storage import Storage
 
 
-class ExceptionHandler(QObject):
+class VisibleExceptionHandler(QObject):
     _instance = None
 
     def __new__(cls):
         if not isinstance(cls._instance, cls):
             cls.storage_signal = Signal()
-            cls._instance = super(ExceptionHandler, cls).__new__(cls)
+            cls._instance = super(VisibleExceptionHandler, cls).__new__(cls)
         return cls._instance
 
     def __init__(self):
